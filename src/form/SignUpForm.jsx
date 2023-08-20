cf65c71d32f74e2593f4c36acbb9c4acb9a5fdbb
thehieu236 <thehieu236@gmail.com>
2023-08-20 11:11:15 +0700

    fix classname

--- src/form/SignUpForm.jsx
+++ src/form/SignUpForm.jsx
@@ -102,7 +102,7 @@ function SignUpForm() {
           <button
             disabled={Formik.isSubmitting}
             type="submit"
-            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 inline-flex items-center mt-3"
+            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 inline-flex items-center mt-3"
           >
             {Formik.isSubmitting ? (
               <div>
